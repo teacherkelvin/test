4594a77668106777c0b818cb1959595eb79dd754
teacherkelvin <1279903434@qq.com>
2018-12-29 09:53:31 +0800

    add next line

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test
 {
 	public static void main(String[] args) {
-		System.out.println("hello git");	
+		System.out.println("hello git\n");
+		System.out.println("next line\n");
+
 	}
 }
